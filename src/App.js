662943c4d5bf26b3fd6d9c9f5555705d2334c44d
NaileaT <138537144+NaileaT@users.noreply.github.com>
2023-08-21 18:16:36 -0600

    Clase 3 completada

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 
 import Container from "./Components/Container";
 import Header from "./Components/Header";
+import GlobalStyled from "./GlobalStyled";
 
 function App() {
   return (
     <>
+      <GlobalStyled/>
       <Header />
       <Container />
     </>
