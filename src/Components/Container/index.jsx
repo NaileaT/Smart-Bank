040af1b6c40b9777e7444cfc534eb3e86dc11060
NaileaT <138537144+NaileaT@users.noreply.github.com>
2023-08-22 18:29:38 -0600

    Clase 4 completada

--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components"
 import Title from "../Title";
 import Account from "../Account";
+import List from "../List";
 
 
 const StyledContainer = styled.div`
@@ -27,6 +28,7 @@ const Container = () => {
       <Title>Smart Bank</Title>
       <StyledContent>
         <Account />
+        <List/>
       </StyledContent>
     </StyledContainer>
   );
